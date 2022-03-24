c925145745b801b99b0d5d183057ab2631d81994
joejiang111 <jyhjoe@gmail.com>
2022-03-24 14:20:56 -0400

    1

---
+++ chart.js
@@ -0,0 +1,11 @@
+
+
+
+
+var rect = d3.select("#chart1")
+            .enter().append("rect")
+            .attr("x", 100)
+            .attr("y", 100)
+            .attr("width", 100)
+            .attr("height", 100)
+            .attr("fill", "red");
